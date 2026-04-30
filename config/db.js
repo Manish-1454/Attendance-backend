267103075c7c91880b685676ef746339c2588499
Manish-1454 <manishthorappadi2@gmail.com>
2026-04-30 10:16:39 +0530

    db.js changes

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB Connected");
-  } catch (err) {
-    console.error("❌ DB Error: ", err.message);
-    process.exit(1); // stop the app if DB fails
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected ✅`);
+  } catch (error) {
+    console.error(`DB Error: ${error.message}`);
+    process.exit(1);
   }
 };
 

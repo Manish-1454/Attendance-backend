995bf21e0f2d1b5aee2a5dda8f8fdb0fe0573afb
Manish-1454 <manishthorappadi2@gmail.com>
2026-04-30 11:31:19 +0530

    fix server cors

--- server.js
+++ server.js
@@ -13,13 +13,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization']
-}));
-
-app.options('*', cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
